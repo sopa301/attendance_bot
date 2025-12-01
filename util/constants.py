8d7157ebaa004f1c1101ce01cae1dfce47cb9822
ryan poon <sopa301@users.noreply.github.com>
2025-12-01 16:38:34 +0800

    Increase limits

--- util/constants.py
+++ util/constants.py
@@ -5,25 +5,27 @@ PRESENT_SYMBOL = "✅"
 ABSENT_SYMBOL = "❌"
 CANCELLATION_SYMBOL = "🚫"
 
-SIGN_UP_SYMBOL="✅"
-DROP_OUT_SYMBOL="❌"
+SIGN_UP_SYMBOL = "✅"
+DROP_OUT_SYMBOL = "❌"
 
 ACTIVE_SYMBOL = "🟢"
 INACTIVE_SYMBOL = "🔴"
 
-class Membership(Enum):
-  REGULAR = 0
-  NON_REGULAR = 1
 
-  @classmethod
-  def from_data_string(string):
-    return Membership(int(string))
-  
-  def to_representation(self):
-    return "Regular" if self == Membership.REGULAR else "Non-Regulars"
-  
-  def to_db_representation(self):
-    return "regulars" if self == Membership.REGULAR else "non_regulars"
+class Membership(Enum):
+    REGULAR = 0
+    NON_REGULAR = 1
+
+    @classmethod
+    def from_data_string(string):
+        return Membership(int(string))
+
+    def to_representation(self):
+        return "Regular" if self == Membership.REGULAR else "Non-Regulars"
+
+    def to_db_representation(self):
+        return "regulars" if self == Membership.REGULAR else "non_regulars"
+
 
 # "Ban"anas constants
 ABSENT_POINTS = 3
@@ -36,4 +38,4 @@ PENALISE_NON_REGULARS = True
 PENALISE_STANDINS = False
 
 # Logistics constants
-MAX_PEOPLE_PER_SESSION = 24
+MAX_PEOPLE_PER_SESSION = 200
